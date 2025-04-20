2cea26f8ac135db674407c87a0e77dc2576e41df
GilmarBetancourt <gilmarbeta@hotmail.com>
2025-04-19 23:07:00 -0400

    Hack 9 finished.

--- hack_9.py
+++ hack_9.py
@@ -5,5 +5,14 @@ text: {"foo":"fookziman","bar":"barziman"} output => {"Foo":"Fooziman"}
 
 def fn_hack_9(s):
     result = s
-    #...
+    for key in list(result.keys()):
+        if(key=="foo"):
+            result[key] = "Fooziman"
+            result["Foo"] = result.pop(key)
+        else:
+            result.pop(key)
+    print(result)
     return result
+
+
+fn_hack_9({"foo":"fookziman","bar":"barziman"})
